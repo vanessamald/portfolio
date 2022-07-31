869500364a72c81e42e2ea59f6b44fc71a16f39d
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-07-31 01:15:22 -0500

    adding link to resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import resumePreview from "../../assets/images/Vanessa_Resume_Preview.jpg";
+import resumePreview from "../../assets/images/Vanessa_Resume_Preview.jpggit ";
 
 function Resume () {
     return (
         <div className="resume-container">
             <div className="resume-content">
                 <div className="resume-item">
-                    <a className="resume-link" href="">Resume</a>
+                    <a className="resume-link" href="https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:4b6af2cc-b459-3e23-bef6-cad7f0f7a782">Resume</a>
                     <img src={resumePreview}></img>
                 </div>
             </div>
