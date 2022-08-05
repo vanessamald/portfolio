019b648bf18e15ad82499dcecdb3f9aa8e3fd22a
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-08-04 23:42:37 -0500

    updating nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -82,8 +82,8 @@ const scrollToRef = useRef();
           </Modal.Header>
           <Modal.Body>
                 <a onClick={() => window.location.replace("/portfolio/#about")} className="nav-link-right">About</a> 
-                <a onClick={() => window.location.replace("/#resume")} className="nav-link-right">Resume</a> 
-                <a onClick={() => window.location.replace("/#portfolio")} className="nav-link-right" >Portfolio</a>
+                <a onClick={() => window.location.replace("/portfolio/#resume")} className="nav-link-right">Resume</a> 
+                <a onClick={() => window.location.replace("/portfolio/#portfolio")} className="nav-link-right" >Portfolio</a>
           </Modal.Body>
         </Modal>
         </div>
